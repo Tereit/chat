0ed2ad6ccb5c75dca0a05ce6cf045cbf53f5e03d
Tereit <morten_mulvik@hotmail.com>
2018-06-12 12:21:00 +0200

    added a todo comment, reminder to fix a bug in a test

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// TODO: error thrown by this is something to do with firebase auth, investigate issue further
 xit('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
